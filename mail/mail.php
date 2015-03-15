eaec8b1047a9189158cd1cdece423405c1ae4b09
Ivan Burlaka <ivan.burlaka@mail>
2015-03-15 17:57:24 +0200

    vk

--- mail/mail.php
+++ mail/mail.php
@@ -4,16 +4,17 @@
 	$adress = $_POST["adress"];
 	$phone = $_POST["phone"];
 	$size = $_POST["size"];
-	$email_body = "";
+	$email_body = "Yo, Bitch!";
 	$email_body ="Name: " . $name . "\n" . "adress: " . $adress . "\n" . "phone: " . $phone ."\n". "size: " . $size;
 	echo $email_body;
-	// send mail
-	    $emailTo ="ivanburlakas@gmail.com";
+	send mail
+	    $emailTo ="studmuffin.info@gmail.com";
 	   	$subject = "I hope this works!";
 	   	
 	    $headers="From: rob@robpercival.co.uk";
-
-	   	mail($emailTo, $subject, $email_body, $headers);
+	    if(mail($emailTo, $subject, $email_body, $headers)) {
+	    	echo "Yo!";
+	   	}
 
  ?>
 </pre>
